1dea79a0faa56986f13c3c78b91010913fbbd2b2
Allan Muturi <allan.muturi@elige-africa.com>
2024-05-08 20:32:42 +0300

    feat: add search component

---
+++ EMovies/View/components/SearchComponent.swift
@@ -0,0 +1,31 @@
+//
+//  SearchComponent.swift
+//  EMovies
+//
+//  Created by Allan on 5/8/24.
+//
+
+import SwiftUI
+
+struct SearchComponent: View {
+    
+    @StateObject var homeData = HomeViewModel()
+    
+    var body: some View {
+        HStack(spacing: 5){
+            Image(systemName: "magnifyingglass")
+            TextField("Search for Movies or Tv Series", text: $homeData.searchQuery)
+            Button(action: search, label: {
+                Text("Search")
+            }).buttonStyle(.borderedProminent)
+        }.padding(.vertical, 10).padding(.horizontal).background(Color.white)
+            .shadow(color: Color.black.opacity(0.06), radius: 5, x:5, y: 5)
+            .shadow(color: Color.black.opacity(0.06), radius: 5, x: -5, y: -5)
+    }
+}
+
+func search(){}
+
+#Preview {
+    SearchComponent()
+}
---
+++ EMovies/ViewModel/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  EMovies
+//
+//  Created by Allan on 5/8/24.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject{
+    
+    @Published var searchQuery = ""
+}
