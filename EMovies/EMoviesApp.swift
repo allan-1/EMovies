61f380bc7d9ab8e02ebedc1d5c69bce557da8ca9
Allan Muturi <allan.muturi@elige-africa.com>
2024-05-08 16:45:10 +0300

    initial commit

---
+++ EMovies/EMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMoviesApp.swift
+//  EMovies
+//
+//  Created by Allan on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct EMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
