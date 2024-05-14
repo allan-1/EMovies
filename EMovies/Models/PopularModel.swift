14b4f24b6afb64232fa4ae090edde66edd4372a1
Allan Muturi <allan.muturi@elige-africa.com>
2024-05-14 14:05:32 +0300

    refactor: popular model

--- EMovies/Models/PopularModel.swift
+++ EMovies/Models/PopularModel.swift
@@ -11,34 +11,39 @@ struct PopularModel: Codable {
     let page: Int
     let results: [Results]
     let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
 }
 
+// MARK: - Result
 struct Results: Codable {
     let adult: Bool
-    let gender, id: Int
-    let knownForDepartment: String?
-    let name, originalName: String
-    let popularity: Double
-    let profilePath: String?
-    let knownFor: [KnownFor]
-}
-
-struct KnownFor: Codable {
     let backdropPath: String?
-    let id: Int
-    let originalTitle: String?
-    let overview: String
-    let posterPath: String?
-    let mediaType: MediaType
-    let adult: Bool
-    let title: String?
-    let originalLanguage: String
     let genreIDS: [Int]
+    let id: Int
+    let originalLanguage, originalTitle, overview: String
     let popularity: Double
-    let releaseDate: String?
-    let video: Bool?
+    let posterPath, releaseDate, title: String
+    let video: Bool
     let voteAverage: Double
     let voteCount: Int
-    let originalName, name, firstAirDate: String?
-    let originCountry: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
 }
