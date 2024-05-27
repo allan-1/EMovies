7d5839dc8d195ca6521c5a8bb2545ec15a5cf12f
Allan Muturi <allan.muturi@elige-africa.com>
2024-05-27 18:16:57 +0300

    feat: create movieListView

--- EMovies/View/MovieListView.swift
+++ EMovies/View/MovieListView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct MovieListView: View {
+    var genreId: Int
+    var genreTitle: String
+    @StateObject var homeViewModel = HomeViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack{
+                SearchComponent(screenPlaceholder: "Movies")
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading).padding()
+            if let fetchedmovies = homeViewModel.fetchedMoviesByGenre {
+                if fetchedmovies.results.isEmpty{
+                    Text("No movies")
+                }else{
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 170))]) {
+                        ForEach(fetchedmovies.results, id: \.id) { result in
+                        NavigationLink(destination: MovieDetail(movieId: result.id, movieTitle: result.originalTitle)){
+                                MovieItem(releaseYear: result.releaseDate, backdropPath: result.backdropPath ?? result.posterPath, movieTitle: result.originalTitle, mediaType: "movie")
+                            }
+                        }
+                    }
+
+                }
+            }else{
+                ProgressView()
+            }
+        }.onAppear(perform: {
+            homeViewModel.getMovieListByGenre(genreId: genreId)
+        }).navigationTitle(genreTitle)
     }
 }
 
 #Preview {
-    MovieListView()
+    MovieListView(genreId: 12, genreTitle: "Action")
 }
